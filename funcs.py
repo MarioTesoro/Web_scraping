212f9ce6a8e94bd3af6325e8a272fa87d2d333b8
MarioTesoro <62204045+MarioTesoro@users.noreply.github.com>
2021-03-30 21:23:16 +0200

    infinite scroll

--- funcs.py
+++ funcs.py
@@ -5,8 +5,9 @@ import urllib.parse
 from urllib.parse import ParseResult, urljoin
 import pywebcopy
 import pathlib
-import cssutils 
+import cssutils
 import re
+import time
 
 def getDownloadPath():
     download_path = os.path.dirname(os.path.realpath(__file__))
@@ -17,41 +18,80 @@ def parseUrl(url):
     print(parsed_url.netloc)
     return parsed_url.netloc
 
-def HTMLparser(url):
+def HTMLparser(page,url):
     print("Beginning html parsing")
     htmlPath = set()
+    alts = set()
     folder=parseUrl(url)
     try:
         os.mkdir(os.path.join(os.getcwd(), folder))
     except:
         pass
     os.chdir(os.path.join(os.getcwd(), folder))
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    
+    soup = BeautifulSoup(page, 'html.parser')
+    aTag = soup.find_all('a')
     images = soup.find_all('img',alt=True)
+    aTags =soup.findAll('a')
     i=0
-    for image in images:
-        name=i    
+    for image in images:  
         link = image['src']
         print('link: ',link)
         alt = image.get('alt','')
         print('alt',alt)
-        if ("http:" in link) or ("https:" in link):
+        if (link[:5] == "http:" ) or (link[:6] =="https:"):
             htmlPath.add(link)
+            alts.add(alt)
+        elif(link[:1]== "/"):
+            htmlPath.add(url+link)
+            alts.add(alt)
         else:
-            htmlPath.add(url+"/"+link)
-        i=i+1
-    print(htmlPath)
-    return htmlPath
+            htmlPath.add(link)
+            alts.add(alt)
+    #funzioni
+    """for aTag in aTags:
+        href = aTag['href']
+        print("href: "+ str(href))
+        if (link[:5] == "http:" ) or (link[:6] =="https:"):
+            htmlPath.add(link)
+        elif(link[:1]== "/"):
+            htmlPath.add(url+link)
+        else:
+            htmlPath.add(link)"""
 
-def resourcesDown(url):
-    filename = os.path.basename(url)
-    print(filename)
-    """with open(filename, 'wb') as f:
-            im = requests.get(url)
-            f.write(im.content)
-            print('Writing: ', filename)"""
+    print(htmlPath)
+    return htmlPath,alts
+
+def resourcesDown(url,counter):
+    im = requests.get(url)
+    if(im.ok):
+        format = None
+        filename = os.path.basename(url)
+        print("Filename: "+filename)
+        if('jpg' in filename):
+            format ='.jpg'
+        elif('png' in filename):
+            format ='.png'
+        elif('jpeg' in filename):
+            format ='.jpeg'
+        elif('mp4' in filename):
+            format= '.mp4'
+        elif('svg' in filename):
+            format='.svg'
+        else:
+            format ='.jpg'
+            
+        if(format !=None):
+            stringedCounter = str(counter)
+            print(stringedCounter + format)
+            """with open(stringedCounter + format, 'wb') as f:
+                    f.write(im.content)
+                    print('Writing: ', stringedCounter + format)
+                    f.close"""
+    else:
+        print(im.status_code)     
+    return True
+   
+
 
 def sourceCodeDownloader(url,download_path): 
     folder=parseUrl(url)
@@ -66,7 +106,7 @@ def sourceCodeDownloader(url,download_path):
     except:
         return False
     return True
-
+"""
 def cssParseURLS(url,download_path):
     folderName = parseUrl(url)
     parser = cssutils.CSSParser()
@@ -79,23 +119,65 @@ def cssParseURLS(url,download_path):
         urls = re.findall("url\(.+?\)",stringedSheet)
         print(urls)
         print('--------------------------------------------------------------------------------------')
+        """
         
-def findCssSheets(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
+def findCssSheets(url,page):
+    soup = BeautifulSoup(page, 'html.parser')
     cssPath = set()
     links = soup.find_all('link')
     sheets = []
     for link in links:
-        attribute = link.attrs.get("rel")
-        if(attribute[0] == 'stylesheet'):
-            href = link.attrs.get("href")
+        href = link.attrs.get("href")
+        if('.css' in href):  
             if ("http:" in href) or ("https:" in href):
                 cssPath.add(href)
             else:
                 cssPath.add(url+"/"+href)
-            print(cssPath)
+            #print(cssPath)
     return cssPath
 
 
-#cssParseURLS('https://brave-goldberg-4b2f82.netlify.app','C://Users//39320//Desktop//Web_scraping')
+def scroll(driver , timeout,safetytime):
+        scroll_pause_time = timeout
+        beginTime= time.time()
+
+        # Get scroll height
+        last_height = driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            print(time.time()-beginTime)
+            if(time.time()-beginTime >= safetytime):
+                return
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            try:
+                time.sleep(scroll_pause_time)
+            except:
+                print("need more timeout")
+                return
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            print(new_height, last_height)
+            if abs(new_height - last_height)<=5:#fixing?
+                # If heights are the same it will exit the function
+                return
+            last_height = new_height
+
+def cssParser(sheetURL,mainURL):
+    r = requests.get(sheetURL)
+    css = r.content
+    urls = []
+    cssURLS = set()
+    #stringedSheet = str(css)
+    urls = re.findall('url\(([^)]+)\)',str(css))
+    for res in urls:
+        if (res[:5] == "http:" ) or (res[:6] == "https:"):
+            cssURLS.add(res)
+        elif(res[:3] == "../"):
+            cssURLS.add(mainURL+"/"+res[3:])
+        elif(res[:2] =="./"):
+            cssURLS.add(mainURL+"/"+res[2:])
+    return cssURLS
--- main.py
+++ main.py
@@ -1,25 +1,61 @@
 import funcs
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+
 from webdriver_manager.chrome import ChromeDriverManager
+import requests
+
+
 
 #url = input("Type website url: ")
 print("Web scraping analyisis")
-urls=['https://brave-goldberg-4b2f82.netlify.app'] #'https://unsplash.com/'
+urls=['https://it.wikipedia.org/wiki/Pagina_principale'] #'https://unsplash.com/' #'https://brave-goldberg-4b2f82.netlify.app' #'https://twitter.com/Twitter?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'
+cssURLS= set()
+htmlURLS = set()
+alts = set()
 downlaod_path = funcs.getDownloadPath()
 print(downlaod_path)
-#driver = webdriver.Chrome(ChromeDriverManager().install())
-for url in urls: 
-    #driver.get(url)
-    #element= driver.find_element_by_tag_name('body')
-    #element.send_keys(Keys.END)
-    #downlaod source code
-    #funcs.sourceCodeDownloader(url,downlaod_path)
-    #css analisis
-    #sheets = funcs.findCssSheets(url)
-    #funcs.cssParseURLS(url,downlaod_path)
-    #downlaod files
-    htmlSheets = funcs.HTMLparser(url)
-    #close driver
-    #driver.close()    
+driver = webdriver.Chrome(ChromeDriverManager().install())
+for url in urls:
+    driver.get(url)
+    driver.maximize_window()
+    print(len(str(driver.page_source)))
+    
+    try:
+        element = driver.find_element_by_tag_name('body')
+        safetytime = 30
+        loadingTime= 10
+        funcs.scroll(driver,loadingTime,safetytime)
+        page = driver.page_source
+        print(len(str(page)))
+        sheets = funcs.findCssSheets(url,page)
+        #print(len(sheets))
+        if(len(sheets)> 0):
+            for sheetURL in sheets:
+                print(sheetURL)
+                cssURLS.update(funcs.cssParser(sheetURL,url)) 
+                print("---------------------------------------------------------------------\n")
+        else:
+            print("css not found")
+        #downlaod source code
+        #funcs.sourceCodeDownloader(url,downlaod_path)
+        htmlURLS,alts = funcs.HTMLparser(page,url)
+        #join set() css e html
+        htmlURLS.update(cssURLS)
+        print(htmlURLS)
+        print("---------------------------------------------------------------------\n")
+        print(alts)
+        counter=1
+        for resource in htmlURLS:
+            try:
+                result=funcs.resourcesDown(resource,counter)
+            except:
+                print("exception")
+            counter=counter+1
+    except Exception as e:
+        print("time exceeded")
+    finally:
+        #downlaod files
+        #htmlSheets = funcs.HTMLparser(url)
+        #close driver
+        driver.close()    
